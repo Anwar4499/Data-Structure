6511d0454071332649151bb1de1dd0596e00be69
Raman-sh <72217209+Raman-sh@users.noreply.github.com>
2020-10-13 10:56:05 +0530

    Add files via upload
    added file

---
+++ access specifires.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class addition
+{
+public:
+	int a,b,c;
+	public:
+		void nala()
+		{
+		//	cout<<"\nenter the value=";
+		//	cin>>a;
+           
+			cout<<"\nenter the value=";
+			cin>>b;
+ c=a+b;
+ cout<<"\nvalue is="<<c;
+		}
+};
+main()
+{
+	addition a;
+	a.a=10;
+	a.nala();
+}
